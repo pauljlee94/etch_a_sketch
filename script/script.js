186bfca3a812d59e0773ac3368622211357b3a88
Paul Lee <pauljlee94@gmail.com>
2019-04-11 14:42:26 -0400

    Add 'sketch' feature

--- script/script.js
+++ script/script.js
@@ -1,4 +1,7 @@
 const container = document.querySelector('#container');
+const resetButton = document.querySelector('.resetButton')
+let grid = document.getElementsByClassName('grid')
+
 function makeGrid (x) { 
     for (i=0; i<x; i++) {
         for (j=0; j<x; j++) {
@@ -7,14 +10,39 @@ function makeGrid (x) {
             grid.style.height = dimension + "px";
             grid.style.width = dimension + "px";
             if (i%2==0 && j%2==0 || i%2!=0 && j%2!=0) {
-                grid.classList.add('grid1');
+                grid.classList.add('grid', 'white');
                 container.appendChild(grid);
             }else {
-                grid.classList.add('grid2');
+                grid.classList.add('grid', 'gray');
                 container.appendChild(grid);
             }
         }
     }
 }
 
-makeGrid(20)
+function reset() {
+    container.innerHTML = ""
+    let x = prompt("size?")
+    if (x > 0) {
+        makeGrid(x)
+    }else {
+        alert("Invalid entry")
+        reset()
+    }
+}
+
+resetButton.addEventListener('click', function() {
+    reset();
+    gridEventListener()
+})
+
+function gridEventListener () {
+    for (let i=0; i<grid.length; i++) {
+        grid[i].addEventListener('mouseenter', function() {
+            grid[i].style.backgroundColor = 'black';
+    })}
+}
+
+//Initialize Game
+reset()
+gridEventListener()
